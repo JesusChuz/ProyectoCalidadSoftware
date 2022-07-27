4ae93a3be690675aeae14e20e3d537a76273aee8
josebrenes99 <98366183+josebrenes99@users.noreply.github.com>
2022-07-26 18:53:21 -0600

    Add files via upload

--- reclutamiento.php
+++ reclutamiento.php
@@ -16,51 +16,54 @@ function executeTestCase($caps) {
         $caps
   );
 
+  // This is where Selenium server 2/3 listens by default. For Selenium 4, Chromedriver or Geckodriver, use http://localhost:4444/
+/*$host = 'http://localhost:4444/wd/hub';
+$capabilities = DesiredCapabilities::chrome();
+$driver = RemoteWebDriver::create($host, $capabilities);
+*/
+
 $driver->manage()->window()->maximize();
 
-    // entramos a la pagina donde vamos hacer las pruebas en OrangeHRM
-$driver->get("https://opensource-demo.orangehrmlive.com/index.php/recruitment/viewCandidates");
-// click | id=txtUsername | 
-$driver->findElement(WebDriverBy::id("txtUsername"))->click();
-// type | id=txtUsername | admin
-$driver->findElement(WebDriverBy::id("txtUsername"))->sendKeys("admin");
+// entramos a la pagina donde vamos hacer las pruebas en OrangeHRM
+ // open | https://opensource-demo.orangehrmlive.com/index.php/auth/validateCredentials | 
+$driver->get("https://opensource-demo.orangehrmlive.com/index.php/auth/validateCredentials");
+// click | xpath=(.//*[normalize-space(text()) and normalize-space(.)='LOGIN Panel'])[1]/following::span[1] | 
+$driver->findElement(WebDriverBy::xpath("(.//*[normalize-space(text()) and normalize-space(.)='LOGIN Panel'])[1]/following::span[1]"))->click();
+// type | id=txtUsername | Admin
+$driver->findElement(WebDriverBy::id("txtUsername"))->sendKeys("Admin");
+// click | id=txtPassword | 
+$driver->findElement(WebDriverBy::id("txtPassword"))->click();
 // type | id=txtPassword | admin123
 $driver->findElement(WebDriverBy::id("txtPassword"))->sendKeys("admin123");
-// submit | id=frmLogin | 
-$driver->findElement(WebDriverBy::id("frmLogin"))->submit();
+// click | id=btnLogin | 
+$driver->findElement(WebDriverBy::id("btnLogin"))->click();
+// click | xpath=(.//*[normalize-space(text()) and normalize-space(.)='Projects'])[1]/following::b[1] | 
+$driver->findElement(WebDriverBy::xpath("(.//*[normalize-space(text()) and normalize-space(.)='Projects'])[1]/following::b[1]"))->click();
 // click | id=btnAdd | 
 $driver->findElement(WebDriverBy::id("btnAdd"))->click();
-// click | id=candidates_description | 
-$driver->findElement(WebDriverBy::id("candidates_description"))->click();
-// type | id=candidates_description | Anexion
-$driver->findElement(WebDriverBy::id("candidates_description"))->sendKeys("Anexion");
-// click | xpath=(.//*[normalize-space(text()) and normalize-space(.)='Add Candidates'])[1]/following::ol[1] | 
-$driver->findElement(WebDriverBy::xpath("(.//*[normalize-space(text()) and normalize-space(.)='Add Candidates'])[1]/following::ol[1]"))->click();
-// click | xpath=(.//*[normalize-space(text()) and normalize-space(.)='*'])[2]/following::img[1] | 
-$driver->findElement(WebDriverBy::xpath("(.//*[normalize-space(text()) and normalize-space(.)='*'])[2]/following::img[1]"))->click();
-// click | link=25 | 
-$driver->findElement(WebDriverBy::linkText("25"))->click();
-// click | id=candidates_recurring | 
-$driver->findElement(WebDriverBy::id("candidates_recurring"))->click();
-// click | id=candidates_length | 
-$driver->findElement(WebDriverBy::id("candidates_length"))->click();
-// click | id=saveBtn | 
-$driver->findElement(WebDriverBy::id("saveBtn"))->click();
-// click | link=Anexion | 
-$driver->findElement(WebDriverBy::linkText("Anexion"))->click();
-// click | id=candidates_description | 
-$driver->findElement(WebDriverBy::id("candidates_description"))->click();
-// type | id=candidates_description | Anexion Guanacaste
-$driver->findElement(WebDriverBy::id("candidates_description"))->clear();
-$driver->findElement(WebDriverBy::id("candidates_description"))->sendKeys("Anexion Guanacaste");
-// click | id=saveBtn | 
-$driver->findElement(WebDriverBy::id("saveBtn"))->click();
-// click | id=ohrmList_chkSelectRecord_27 | 
-$driver->findElement(WebDriverBy::id("ohrmList_chkSelectRecord_27"))->click();
-// click | id=btnDelete | 
-$driver->findElement(WebDriverBy::id("btnDelete"))->click();
-// click | id=dialogDeleteBtn | 
-$driver->findElement(WebDriverBy::id("dialogDeleteBtn"))->click();
+// click | id=addCandidate_firstName | 
+$driver->findElement(WebDriverBy::id("addCandidate_firstName"))->click();
+// type | id=addCandidate_firstName | Juan
+$driver->findElement(WebDriverBy::id("addCandidate_firstName"))->sendKeys("Juan4");
+// click | id=addCandidate_lastName | 
+$driver->findElement(WebDriverBy::id("addCandidate_lastName"))->click();
+// type | id=addCandidate_lastName | Ramirez
+$driver->findElement(WebDriverBy::id("addCandidate_lastName"))->sendKeys("Ramirez4");
+// click | id=addCandidate_email | 
+$driver->findElement(WebDriverBy::id("addCandidate_email"))->click();
+// type | id=addCandidate_email | jramirez@gmail.com
+$driver->findElement(WebDriverBy::id("addCandidate_email"))->sendKeys("jramirez@gmail.com");
+// click | id=addCandidate_vacancy | 
+$driver->findElement(WebDriverBy::id("addCandidate_vacancy"))->click();
+// select | id=addCandidate_vacancy | label=Software Engineer
+//$driver->findElement(WebDriverBy::id("addCandidate_vacancy"))->selectByVisibleText("Software Engineer");
+$selectElement = $driver->findElement(WebDriverBy::id('addCandidate_vacancy'));
+$select = new WebDriverSelect($selectElement); 
+$selectedOptions = $select->getAllSelectedOptions();
+// click | id=btnSave | 
+$driver->findElement(WebDriverBy::id("btnSave"))->click();
+
+$driver->get("https://opensource-demo.orangehrmlive.com/index.php/auth/validateCredentials");
 
 $driver->quit();
 
@@ -72,8 +75,8 @@ $caps = array(
         "browserName" => "Chrome",
         "version" => "103.0",
         "resolution" => "1920x1080",
-        "build" => "Testreclutamiento Build",
-        "name" => "Test reclutamiento"
+        "build" => "Build Test insertar candidato",
+        "name" => "Test insertar candidato"
     )
   );
 foreach ( $caps as $cap ) {
